b3a359b7584f836dad62f22c9f530abf4edd2497
jcjc1233 <jeanclaudeabouelias@gmail.com>
2024-10-26 20:31:22 -0400

    initial commit
    commit front end pages

---
+++ front-end/src/TimelineSection.js
@@ -0,0 +1,16 @@
+// src/components/TimelineSection.js
+import React from "react";
+//import './TimelineSection.css';
+
+const TimelineSection = () => {
+  return (
+    <section className="timeline-section text-center my-5">
+      <div className="timeline-placeholder">
+        <p>Put image of timeline here</p>
+        <p>Also this should link to the timeline page</p>
+      </div>
+    </section>
+  );
+};
+
+export default TimelineSection;
