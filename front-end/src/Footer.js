91bc59a0032f1f6848ba99be3cbec88e614af807
jcjc1233 <jeanclaudeabouelias@gmail.com>
2024-10-26 20:35:37 -0400

    Update Footer.js

---
+++ front-end/src/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer bg-danger text-white text-center p-4">
+      <p className="footer-text">
+        TrackMyDegree helps Concordia Engineering students visualize and plan
+        their courses with an easy-to-use and interactive UI.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
