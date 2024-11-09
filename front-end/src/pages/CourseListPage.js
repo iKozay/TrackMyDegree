684e9c24233d7ac6f2d713d34b82848df1dfc98c
XavierKMT <78317478+XavierKMT@users.noreply.github.com>
2024-11-08 22:55:47 -0500

    minor clean up and comments

--- front-end/src/pages/CourseListPage.js
+++ front-end/src/pages/CourseListPage.js
@@ -1,12 +1,5 @@
 import React, { useEffect, useState } from "react";
-import computerScienceGroup from "../course data/soen_courses/computer_science_group.json";
-import soenElectives from "../course data/soen_courses/soen_electives.json";
-import soenCore from '../course data/soen_courses/soen_core.json';
-import engineeringNaturalScience from '../course data/soen_courses/engineering_natural_science_group.json';
-import engineeringCore from '../course data/engineering_core.json';
-import humanities from '../course data/general_education_electives/humanities.json';
-import otherStudies from '../course data/general_education_electives/other_complementary_studies.json';
-import socialSciences from '../course data/general_education_electives/social_sciences.json';
+import soenCourses from "../course data/soen_courses";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -16,18 +9,6 @@ import Dropdown from "react-bootstrap/Dropdown";
 import CourseListAccordion from "../components/CourseListAccordion";
 
 const degrees = ['Software Engineering', 'Computer Engineering', 'Electrical Engineering'];
-const genEdElec = [
-  {title: 'Humanities', courseList: humanities},
-  {title: 'Social Sciences', courseList: socialSciences},
-  {title: 'Other Complementary Studies', courseList: otherStudies},
-];
-const soenCourses = [
-  {title: 'Engineering Core', credits: 30.5, courseList: engineeringCore, subcourseTitle: 'General Education Electives', subcourseCredits: 3, subcourses: genEdElec},
-  {title: 'Software Engineering Core', credits: 47.5, courseList: soenCore},
-  {title: 'Computer Science Group', credits: 23, courseList: computerScienceGroup},
-  {title: 'Engineering and Natural Science Group', credits: 3, courseList: engineeringNaturalScience},
-  {title: 'Software Engineering Electives', credits: 16, courseList: soenElectives, elective: true},
-];
 
 function CourseListPage () {
 
@@ -43,6 +24,7 @@ function CourseListPage () {
         setSelectedCourse(null);
         break;
       case 'Computer Engineering':
+        //courses for other degrees have not been scraped yet, but we will expand our databases throughout the next releases
         setCourseList(null);
         setSelectedCourse(null);
         break;
@@ -75,6 +57,7 @@ function CourseListPage () {
       
       <Row>
         <Col sm={7}>
+          {/* Only display course accordions if the user has selected a degree */}
           {courseList &&
             <CourseListAccordion courseList={courseList} selectedCourse={selectedCourse} setSelectedCourse={setSelectedCourse} />
           }
