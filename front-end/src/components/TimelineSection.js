55967e12b75886a92479aea841add8d28fb882ac
darealstyl <jonahball5@hotmail.com>
2024-11-10 19:51:31 -0500

    added timeline example to main page

--- front-end/src/components/TimelineSection.js
+++ front-end/src/components/TimelineSection.js
@@ -1,12 +1,14 @@
 // src/components/TimelineSection.js
 import React from "react";
+import TimelineExample from "../images/TimelineExample.png";
 
 const TimelineSection = () => {
   return (
     <section className="timeline-section text-center my-5">
       <div className="timeline-placeholder">
-        <p>Put image of timeline here</p>
-        <p>Also this should link to the timeline page</p>
+        {/* <p>Put image of timeline here</p>
+        <p>Also this should link to the timeline page</p> */}
+        <img src={TimelineExample} alt="timeline" />
       </div>
     </section>
   );
