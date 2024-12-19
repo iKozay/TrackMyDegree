c5d9e668ef10ea99cc82a9d84541165370912a60
XavierKMT <78317478+XavierKMT@users.noreply.github.com>
2024-12-18 23:54:24 -0500

    Separated css

--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../AuthContext";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import '../css/Navbar.css';
 
 const Navbar = () => {
   const { isLoggedIn, logout } = useContext(AuthContext);
