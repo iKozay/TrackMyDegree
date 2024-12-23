fd79d84c0d6a1636c9cd9703b2be02f0b9f77bc7
Vraj2301 <93214962+Vraj2301@users.noreply.github.com>
2024-12-23 16:49:16 -0500

    Updated SignIn Page
    Updated SingIn Page css and js file.

--- front-end/src/pages/SignInPage.js
+++ front-end/src/pages/SignInPage.js
@@ -26,50 +26,48 @@ function SignInPage() {
   return (
     <div className="SignInPage">
       <div className="container d-flex justify-content-center align-items-center">
-          <h2 className="text-center mb-4" style={{ fontWeight: 'bold', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>Sign in</h2>
-          <form onSubmit={handleLogin}>
-            <div className="mb-4">
-              <label htmlFor="email" className="form-label">Email:</label>
-              <input
-                type="email"
-                className="form-control"
-                style={{ borderRadius: '100px', borderColor: 'black' }}
-                id="email"
-                placeholder="Enter your email..."
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-            <div className="mb-4">
-              <label htmlFor="password" className="form-label">Password:</label>
-              <input
-                type="password"
-                className="form-control"
-                style={{ borderRadius: '100px', borderColor: 'black'  }}
-                id="password"
-                placeholder="********"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-            <div className="d-flex justify-content-between mb-3">
-              <button
-                type="button"
-                className="btn btn-outline-secondary"
-                style={{ backgroundColor: '#80808040', color: 'black', border: 'none'}}
-                onClick={() => navigate("/")}
-              >
-                Cancel
-              </button>
-              <button type="submit" className="btn btn-primary" 
-              style={{ backgroundColor: 'brown', color: 'white', border: 'none'}}>Submit</button>
-            </div>
-          </form>
-          <div className="text-center">
-            <a href="/signup" className="text-decoration-none">
-              Don't have an account? Register here!
-            </a>
+        <h2 className="text-center mb-4 custom-header">Sign in</h2>
+        <form onSubmit={handleLogin}>
+          <div className="mb-4">
+            <label htmlFor="email" className="form-label">Email:</label>
+            <input
+              type="email"
+              className="form-control custom-input"
+              id="email"
+              placeholder="Enter your email..."
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
+          <div className="mb-4">
+            <label htmlFor="password" className="form-label">Password:</label>
+            <input
+              type="password"
+              className="form-control custom-input"
+              id="password"
+              placeholder="********"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="d-flex justify-content-between mb-3">
+            <button
+              type="button"
+              className="btn btn-outline-secondary custom-cancel-btn"
+              onClick={() => navigate("/")}
+            >
+              Cancel
+            </button>
+            <button type="submit" className="btn btn-primary custom-submit-btn">
+              Submit
+            </button>
+          </div>
+        </form>
+        <div className="text-center">
+          <a href="/signup" className="text-decoration-none">
+            Don't have an account? Register here!
+          </a>
+        </div>
       </div>
     </div>
   );
