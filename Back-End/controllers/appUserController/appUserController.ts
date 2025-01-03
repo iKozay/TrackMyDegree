c1c397addd3a0227eb426c9a746b6d0e4a989e40
pphaoniubi <85983149+pphaoniubi@users.noreply.github.com>
2025-01-03 10:58:44 +0800

    #1 updating logic on appUser endpoints

--- Back-End/controllers/appUserController/appUserController.ts
+++ Back-End/controllers/appUserController/appUserController.ts
@@ -1,64 +1,6 @@
 import Database from "@controllers/DBController/DBController"
 import appUserTypes from "@controllers/appUserController/appUser_types"
 
-async function createAppUser (id: string, email: string, password: string, firstname: string, lastname: string, degree: string, type: appUserTypes.UserType):  Promise<string | undefined> {
-  const conn = await Database.getConnection();
-
-    if(conn){
-        try {
-            // Check if a appUser with the same id already exists
-            const existingAppUser = await conn.request()
-            .input('id', Database.msSQL.VarChar, id)
-            .query('SELECT * FROM AppUser WHERE id = @id');
-        
-            if (existingAppUser.recordset.length > 0) {
-              throw new Error('AppUser with this id already exists.');
-            }
-        
-            await conn.request()
-            .input('id', Database.msSQL.VarChar, id)
-            .input('email', Database.msSQL.VarChar, email)
-            .input('password', Database.msSQL.VarChar, password)
-            .input('firstname', Database.msSQL.VarChar, firstname)
-            .input('lastname', Database.msSQL.VarChar, lastname)
-            .input('degree', Database.msSQL.VarChar, degree)
-            .input('type', Database.msSQL.VarChar, type)
-            .query('INSERT INTO AppUser (id, email, password, firstname, lastname, degree, type) VALUES (?, ?, ?, ?, ?, ?, ?)');
-
-            return `appUser with id: ${id} is created, role: ${type}`;
-          } catch (error) {
-            throw error;
-          } finally {
-            conn.close()
-          }
-    }
-};
-
-
-async function readAppUser (id: string):  Promise<appUserTypes.AppUser | undefined> {
-    const conn = await Database.getConnection();
-    
-      if(conn){
-          try {
-              // Check if a appUser with the id exists
-              const appUser = await conn.request()
-              .input('id', Database.msSQL.VarChar, id)
-              .query('SELECT * FROM AppUser WHERE id = @id');
-          
-              if (appUser.recordset.length === 0) {
-                throw new Error('AppUser with this id does not exist.');
-              }
-          
-              return appUser.recordset[0];
-            } catch (error) {
-              throw error;
-            } finally {
-              conn.close();
-            }
-      }
-  };
-
-
   async function updateAppUser(
         id: string, 
         email: string, 
@@ -152,8 +94,6 @@ async function readAppUser (id: string):  Promise<appUserTypes.AppUser | undefin
   
 //Namespace
 const appUserController = {
-    createAppUser,
-    readAppUser,
     updateAppUser,
     deleteAppUser
 };
