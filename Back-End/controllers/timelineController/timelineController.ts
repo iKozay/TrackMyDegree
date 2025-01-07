8c9d85590a24cd4358675ecf485621ad26a06c16
Shiv2205 <shivamveerabudren@gmail.com>
2025-01-06 22:36:11 -0500

    Timeline Controller added

--- Back-End/controllers/timelineController/timelineController.ts
+++ Back-End/controllers/timelineController/timelineController.ts
@@ -7,9 +7,24 @@ const log = console.log;
 
 async function createTimeline(userTimeline: TimelineTypes.UserTimeline){
   const dbConn = await Database.getConnection();
-  let record: TimelineTypes.TimelineInfo;
+  let records: TimelineTypes.TimelineInfo[] = [];
 
   userTimeline.timeline_items.forEach((item, index) => {
+    records.push(
+      {
+        id          : randomUUID(),
+        course_item : item,
+        user_id     : userTimeline.user_id
+      }
+    );
+
 
   })
 }
+
+
+const timelineController = {
+  createTimeline
+};
+
+export default timelineController;
