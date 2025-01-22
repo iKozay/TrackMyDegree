f6ee86df546430ede152ac437edf90c16fc435dc
Shiv2205 <shivamveerabudren@gmail.com>
2025-01-22 10:21:04 -0500

    Added cascading delete for tables referencing CoursePool records

--- init.sql
+++ init.sql
@@ -33,7 +33,7 @@ CREATE TABLE DegreeXCoursePool (
   creditsRequired INT NOT NULL,
   UNIQUE(degree, coursepool),
   FOREIGN KEY (degree) REFERENCES Degree(id),
-  FOREIGN KEY (coursepool) REFERENCES CoursePool(id)
+  FOREIGN KEY (coursepool) REFERENCES CoursePool(id) ON DELETE CASCADE
 );
 
 CREATE TABLE CourseXCoursePool (
@@ -42,7 +42,7 @@ CREATE TABLE CourseXCoursePool (
   coursepool VARCHAR(255),
   UNIQUE(coursecode, coursepool),
   FOREIGN KEY (coursecode) REFERENCES Course(code), -- Composite foreign key
-  FOREIGN KEY (coursepool) REFERENCES CoursePool(id)
+  FOREIGN KEY (coursepool) REFERENCES CoursePool(id) ON DELETE CASCADE
 );
 
 CREATE TABLE AppUser (  -- Use square brackets for reserved keywords
