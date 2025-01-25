17334a8c58c966fcd230323451e027f2c92be192
cstroeder <stroeder.carter@gmail.com>
2025-01-24 23:45:31 -0500

    Added userpage frontend functionality
    Added user info section and timeline section.
    Created css document for these sections.
    Created test file for frontend functionality.

--- front-end/src/pages/UserPage.js
+++ front-end/src/pages/UserPage.js
@@ -1,9 +1,220 @@
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../css/UserPage.css";
 
 const UserPage = () => {
+  // add way to get user info here
+  const [userInfo, setUserInfo] = useState([
+    { title: "First Name", value: "John" },
+    { title: "Last Name", value: "Doe" },
+    { title: "Email", value: "john.doe@example.com" },
+    { title: "Password", value: "******" },
+    { title: "Degree Concentration", value: "Software Engineer" },
+  ]);
+
+  // add way to get actual list here
+  const degreeConcentrations = [
+    "Software Engineer",
+    "Data Scientist",
+    "Cybersecurity Specialist",
+    "AI Researcher",
+    "Network Engineer",
+  ];
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedUserInfo, setEditedUserInfo] = useState(userInfo.map((item) => item.value));
+
+  const startEditing = () => {
+    setIsEditing(true);
+  };
+
+  const cancelEditing = () => {
+    setEditedUserInfo(userInfo.map((item) => item.value));
+    setIsEditing(false);
+  };
+
+  const saveChanges = () => {
+    // add way to save changes here
+    const updatedInfo = userInfo.map((item, index) => ({
+      ...item,
+      value: editedUserInfo[index],
+    }));
+    setUserInfo(updatedInfo);
+    setIsEditing(false);
+  };
+
+  const handleInputChange = (e, index) => {
+    const updatedValues = [...editedUserInfo];
+    updatedValues[index] = e.target.value;
+    setEditedUserInfo(updatedValues);
+  };
+
+  // add way to get user timelines here
+  const [userTimelines, setUserTimelines] = useState([
+    { id: 1, name: "Timeline 1", modifiedDate: "2025-01-10 10:12am" },
+    { id: 2, name: "Timeline 2", modifiedDate: "2025-01-12 11:43am" },
+    { id: 3, name: "Timeline 3", modifiedDate: "2025-01-14 1:03pm" },
+  ]);
+  const sortedUserTimelines = [...userTimelines].sort((a, b) => new Date(b.modifiedDate) - new Date(a.modifiedDate));
+
+  const [showModal, setShowModal] = useState(false);
+  const [timelineToDelete, setTimelineToDelete] = useState(null);
+
+  const handleDeleteClick = (timeline) => {
+    setTimelineToDelete(timeline);
+    setShowModal(true);
+  };
+
+  const handleDelete = (id) => {
+    setUserTimelines(userTimelines.filter((obj) => obj.id !== id));
+    // add logic to actually delete here
+  };
+
   return (
-    <div>
-        <p> This is the User Page </p>
+    <div className="container-fluid">
+      <div className="row vh-100">
+        
+        {/* Left Side */}
+        <div className="col-12 col-md-4 d-flex flex-column align-items-center border-end text-center mx-auto">
+          <h2 className="mb-4">My Profile</h2>
+          <div className="d-flex flex-column mx-auto">
+            {userInfo.map((item, index) => (
+              <div
+                key={index}
+                className="userInfo-Box d-flex mb-2"
+              >
+                <span className="userinfo-title fw-bold">
+                  {item.title}:
+                </span>
+                {isEditing && item.title === "Degree Concentration" ? (
+                  <select
+                    value={editedUserInfo[index]}
+                    onChange={(e) => handleInputChange(e, index)}
+                    className="form-select"
+                  >
+                    {degreeConcentrations.map((concentration, i) => (
+                      <option key={i} value={concentration}>
+                        {concentration}
+                      </option>
+                    ))}
+                  </select>
+                ) : isEditing ? (
+                  <input
+                    type="text"
+                    className="form-control"
+                    value={editedUserInfo[index]}
+                    onChange={(e) => handleInputChange(e, index)}
+                  />
+                ) : (
+                  <span>{item.value}</span>
+                )}
+              </div>
+            ))}
+          </div>
+          {/* Edit, Cancel, and Save Buttons */}
+          <div className="mt-4">
+            {!isEditing ? (
+              <button className="btn btn-success" onClick={startEditing}>
+                Edit
+              </button>
+            ) : (
+              <div className="d-flex gap-2">
+                <button className="btn btn-secondary" onClick={cancelEditing}>
+                  Cancel
+                </button>
+                <button className="btn btn-danger" onClick={saveChanges}>
+                  Save
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
+
+        {/* Right Side */}
+        <div className="col-12 col-md-6 d-flex flex-column text-center mx-auto mt-3 mt-md-0">
+          <h2 className="mb-4">My Timelines</h2>
+          {sortedUserTimelines.length === 0 ? (
+            <Link to="/timeline_change">
+            <p>You haven't saved any timelines yet, click here to start now!</p></Link>
+          ) : (
+          <div className="list-group">
+            {sortedUserTimelines.map((obj) => (
+            <div
+              key={obj.id}
+              className="timeline-box d-flex align-items-center justify-content-between"
+            >
+              <Link
+                to="/timeline_change" // modify this link once timeline actually hooked up
+                className = "timeline-link"
+              >
+                <span className="timeline-text">{obj.name}</span>
+                <span className="timeline-text">
+                  Last Modified: {obj.modifiedDate}
+                </span>
+              </Link>
+              <button
+                onClick={(e) => {
+                  handleDeleteClick(obj);
+                }}
+                className="timeline-delete btn btn-danger btn-sm"
+              >
+                X
+              </button>
+            </div>
+            ))}
+          </div>)}
+          {/* Add New Timeline Button */}
+          <Link
+            to="/timeline_change"
+            className="timeline-add"
+          >
+            <span className="timeline-text">
+              +
+            </span>
+          </Link>
+        </div>
+      </div>
+      
+      {/* popup */}
+      {showModal && (
+        <div
+          className="modal fade show"
+          tabIndex="-1"
+          style={{ display: 'block' }}
+          aria-labelledby="confirmationModalLabel"
+          aria-hidden="false"
+        >
+          <div className="modal-dialog">
+            <div className="popup-box modal-content">
+              <div className="modal-header d-flex justify-content-center">
+                <h5 className="modal-title" id="confirmationModalLabel">
+                  Delete "{timelineToDelete?.name}"?
+                </h5>
+              </div>
+              <div className="modal-footer d-flex justify-content-center">
+                <button
+                  type="button"
+                  className="btn btn-danger"
+                  onClick={() => setShowModal(false)}
+                >
+                  Cancel
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-success"
+                  onClick={() => {
+                    handleDelete(timelineToDelete);
+                    setShowModal(false);
+                  }}
+                >
+                  Confirm
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
---
+++ front-end/src/test/UserPage.test.js
@@ -0,0 +1,175 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import UserPage from '../pages/UserPage';
+
+describe('Profile Component', () => {
+    test('renders My Profile title', () => {
+        render(<UserPage />);
+        const titleElement = screen.getByText(/My Profile/i);
+        expect(titleElement).toBeInTheDocument();
+    });
+  
+    test('renders user info correctly', () => {
+        const userInfo = [
+            { title: 'First Name', value: 'John' },
+            { title: 'Last Name', value: 'Doe' },
+            { title: 'Email', value: 'john.doe@example.com' },
+            { title: 'Password', value: '******' },
+            { title: 'Degree Concentration', value: 'Software Engineer' },
+        ];
+  
+        render(<UserPage userInfo={userInfo} />);
+        userInfo.forEach((item) => {
+            expect(screen.getByText(item.title)).toBeInTheDocument();
+            expect(screen.getByText(item.value)).toBeInTheDocument();
+        });
+    });
+
+    test('edit button should toggle edit mode', () => {
+        render(<UserPage />);
+        const editButton = screen.getByText(/Edit/i);
+        editButton.click();
+        const cancelButton = screen.getByText(/Cancel/i);
+        const saveButton = screen.getByText(/Save/i);
+        expect(cancelButton).toBeInTheDocument();
+        expect(saveButton).toBeInTheDocument();
+    });
+
+    test('should update the user info when save is clicked', () => {
+        render(<UserPage />);
+        const editButton = screen.getByText(/Edit/i);
+        editButton.click();
+    
+        const inputField = screen.getByLabelText(/First Name/i);
+        fireEvent.change(inputField, { target: { value: 'New Text' } });
+    
+        const saveButton = screen.getByText(/Save/i);
+        saveButton.click();
+    
+        expect(screen.getByText('New Text')).toBeInTheDocument();
+    });
+
+    test('should not update the user info when cancel is clicked', () => {
+        const userInfo = [
+            { title: 'First Name', value: 'John' },
+            { title: 'Last Name', value: 'Doe' },
+            { title: 'Email', value: 'john.doe@example.com' },
+            { title: 'Password', value: '******' },
+            { title: 'Degree Concentration', value: 'Software Engineer' },
+        ];
+
+        render(<UserPage userInfo={userInfo} />);
+        const editButton = screen.getByText(/Edit/i);
+        editButton.click();
+    
+        const inputField = screen.getByLabelText(/First Name/i);
+        fireEvent.change(inputField, { target: { value: 'New Text' } });
+    
+        const cancelButton = screen.getByText(/Cancel/i);
+        cancelButton.click();
+    
+        expect(screen.getByText('John')).toBeInTheDocument();
+    });
+});
+
+
+describe('Timeline Component', () => {
+    test('renders My Timeline title', () => {
+        render(<UserPage />);
+        const titleElement = screen.getByText(/My Timelines/i);
+        expect(titleElement).toBeInTheDocument();
+    });
+  
+    test('renders timeline info correctly', () => {
+        const userTimelines = [
+            { id: 1, name: "Timeline 1", modifiedDate: "2025-01-10 10:12am" },
+            { id: 2, name: "Timeline 2", modifiedDate: "2025-01-12 11:43am" },
+            { id: 3, name: "Timeline 3", modifiedDate: "2025-01-14 1:03pm" },
+        ];
+  
+        render(<UserPage userTimelines={userTimelines} />);
+        userInfo.forEach((item) => {
+            expect(screen.getByText(item.name)).toBeInTheDocument();
+            expect(screen.getByText(item.modifiedDate)).toBeInTheDocument();
+        });
+    });
+
+    test('delete button should trigger popup', () => {
+        const userTimelines = [
+            { id: 1, name: "Timeline 1", modifiedDate: "2025-01-10 10:12am" },
+        ];
+  
+        render(<UserPage userTimelines={userTimelines} />);
+        const deleteButton = screen.getByText(/X/i);
+        deleteButton.click();
+        const titleElement = screen.getByText(/Delete "Timeline 1"?/i);
+        expect(titleElement).toBeInTheDocument();
+        const cancelButton = screen.getByText(/Cancel/i);
+        const confirmButton = screen.getByText(/Confirm/i);
+        expect(cancelButton).toBeInTheDocument();
+        expect(confirmButton).toBeInTheDocument();
+    });
+
+    test('timeline deleted after confirm is clicked', () => {
+        const userTimelines = [
+            { id: 1, name: "Timeline 1", modifiedDate: "2025-01-10 10:12am" },
+        ];
+  
+        render(<UserPage userTimelines={userTimelines} />);
+        const deleteButton = screen.getByText(/X/i);
+        deleteButton.click();
+
+        const confirmButton = screen.getByText(/Confirm/i);
+        confirmButton.click();
+    
+        expect(screen.getByText('Timeline 1')).not.toBeInTheDocument(); 
+    });
+
+    test('timeline not deleted after cancel is clicked', () => {
+        const userTimelines = [
+            { id: 1, name: "Timeline 1", modifiedDate: "2025-01-10 10:12am" },
+        ];
+  
+        render(<UserPage userTimelines={userTimelines} />);
+        const deleteButton = screen.getByText(/X/i);
+        deleteButton.click();
+
+        const cancelButton = screen.getByText(/Cancel/i);
+        cancelButton.click();
+    
+        expect(screen.getByText('Timeline 1')).toBeInTheDocument(); 
+    });
+
+    test('link to create new timeline displayed when none exist for user', () => {
+        const userTimelines = [
+            { id: 1, name: "Timeline 1", modifiedDate: "2025-01-10 10:12am" },
+        ];
+  
+        render(<UserPage userTimelines={userTimelines} />);
+        const deleteButton = screen.getByText(/X/i);
+        deleteButton.click();
+
+        const confirmButton = screen.getByText(/Confirm/i);
+        confirmButton.click();
+    
+        expect(screen.getByText("You haven't saved any timelines yet, click here to start now!")).toBeInTheDocument(); 
+    });
+
+    test('should navigate to timeline page when clicked', () => {
+        const userTimelines = [
+            { id: 1, name: "Timeline 1", modifiedDate: "2025-01-10 10:12am" },
+        ];
+
+        render(
+          <MemoryRouter initialEntries={['/user']}> {/* Set the initial URL */}
+            <UserPage userTimelines={userTimelines}/>
+          </MemoryRouter>
+        );
+      
+        const linkElement = screen.getByText(/Timeline 1/i);
+        fireEvent.click(linkElement);
+      
+        expect(window.location.pathname).toBe('/timeline_change');
+    });
+});
