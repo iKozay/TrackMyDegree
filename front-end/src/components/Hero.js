19b5c6b46c007d060cb4d9af506af8a1517a5aaf
DobDub <hassan.moharram@hotmail.com>
2025-02-16 21:05:15 -0500

    rerouted live demo to correct timeline page

--- front-end/src/components/Hero.js
+++ front-end/src/components/Hero.js
@@ -10,7 +10,7 @@ const Hero = () => {
     <section className="hero text-center my-5">
       <h1 className="hero-title">Organize your course sequence</h1>
       <p className="hero-subtitle">Try Now</p>
-      <button className="btn btn-outline-dark btn-lg hero-button" onClick={() => navigate("/timeline_change")}>
+      <button className="btn btn-outline-dark btn-lg hero-button" onClick={() => navigate("/timeline_initial")}>
         Live Demo{" "}
         <span role="img" aria-label="play">
           ▶️
