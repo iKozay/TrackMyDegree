cab207906cad606747afadbf24ad1dc1444c0da4
XavierKMT <78317478+XavierKMT@users.noreply.github.com>
2025-03-02 22:59:40 -0500

    add typewriter and carousel to landing page

--- front-end/src/pages/LandingPage.js
+++ front-end/src/pages/LandingPage.js
@@ -1,18 +1,36 @@
-// src/components/TimelineSection.js
 import React from "react";
-import Navbar from "../components/Navbar";
-import Hero from "../components/Hero";
-import TimelineSection from "../components/TimelineSection";
-import Footer from "../components/Footer";
+import { useNavigate } from "react-router-dom";
+import '../css/LandingPage.css';
+import ImageCarousel from "../components/ImageCarousel";
+import Typewriter from "typewriter-effect";
 
 const LandingPage = () => {
+
+  const navigate = useNavigate();
+
   return (
-    <section className="">
-      <div className="">
-      <Hero />
-      {/* <TimelineSection /> */}
+    <div className="landing-section">
+      <Typewriter
+        options={{
+          strings: ['Organize your course sequence', 'Plan your degree', 'Visualize your courses', 'Stay on track', 'Navigate your program'],
+          autoStart: true,
+          loop: true,
+          pauseFor: 2000
+        }}
+      />
+      <div className="try-now-section">
+        <p>Try Now!</p>
+        <button className="btn btn-outline-dark btn-lg" onClick={() => navigate("/timeline_initial")}>
+          Live Demo{" "}
+          <span role="img" aria-label="play">
+            ▶️
+          </span>
+        </button>
       </div>
-    </section>
+      <div>
+        <ImageCarousel />
+      </div>
+    </div>
   );
 };
 
