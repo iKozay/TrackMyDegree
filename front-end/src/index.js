a0ee6c733718789d87dc08ee0ba39f6b572a43de
DobDub <hassan.moharram@hotmail.com>
2025-03-11 15:20:43 -0400

    remove strict

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -9,12 +9,12 @@ const options = {
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<React.StrictMode>
-		<PostHogProvider
-			apiKey={process.env.REACT_APP_PUBLIC_POSTHOG_KEY}
-			options={options}
-		>
-			<App />
-		</PostHogProvider>
-	</React.StrictMode>
+	// <React.StrictMode>
+	<PostHogProvider
+		apiKey={process.env.REACT_APP_PUBLIC_POSTHOG_KEY}
+		options={options}
+	>
+		<App />
+	</PostHogProvider>
+	// /* </React.StrictMode> */
 );
