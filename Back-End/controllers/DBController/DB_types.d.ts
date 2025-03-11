545c78739a604b0e7dd4741403a6223304319e44
DobDub <hassan.moharram@hotmail.com>
2025-03-11 01:33:12 -0400

    db timeout fix

--- Back-End/controllers/DBController/DB_types.d.ts
+++ Back-End/controllers/DBController/DB_types.d.ts
@@ -3,15 +3,15 @@ declare namespace SQL {
 
   //Type for config object passed to SQL.connect()
   type Config = {
-    user      : string,
-    password  : string,
-    database  : string,
-    server    : string,
-    options   : {
-                  encrypt                 : boolean,
-                  trustServerCertificate  : boolean, 
-                  requestTimeout          : int
-                },
+    user: string,
+    password: string,
+    database: string,
+    server: string,
+    options: {
+      encrypt: boolean,
+      trustServerCertificate: boolean,
+      requestTimeout: int
+    },
   }
 
 }
