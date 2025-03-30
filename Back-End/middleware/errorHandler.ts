8d8035a86428f80db49639d1a2436f07b7aa95ae
Dimitri Karagiannakis <dkaragiannakis99@gmail.com>
2025-03-30 15:51:19 -0400

    Fixed Sentry DSN and re-added global error tracking in the backend

--- Back-End/middleware/errorHandler.ts
+++ Back-End/middleware/errorHandler.ts
@@ -1,29 +1,29 @@
-import { Request, Response, NextFunction } from "express";
-import createError from "http-errors";
-import HTTP from "@Util/HTTPCodes";
-import Sentry from "./sentryMiddleware";
+import { Request, Response, NextFunction } from 'express';
+import createError from 'http-errors';
+import HTTP from '@Util/HTTPCodes';
+import Sentry from './sentryMiddleware';
 
 // 404 Not Found Middleware
 export const notFoundHandler = (
-	req: Request,
-	res: Response,
-	next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction,
 ) => {
-	next(createError(HTTP.NOT_FOUND, "Page not found!!!"));
-	Sentry.captureException(new Error("Page not found!!!"));
+  next(createError(HTTP.NOT_FOUND, 'Page not found!!!'));
+  Sentry.captureException(new Error('Page not found!!!'));
 };
 
 // Global Error Handler
 export const errorHandler = (
-	err: any,
-	req: Request,
-	res: Response,
-	next: NextFunction
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction,
 ) => {
-	res.status(err.status || HTTP.SERVER_ERR).json({
-		error: err.message || "Internal Server Error",
-	});
-	Sentry.captureException(err);
+  Sentry.captureException(err);
+  res.status(err.status || HTTP.SERVER_ERR).json({
+    error: err.message || 'Internal Server Error',
+  });
 };
 
 export default { notFoundHandler, errorHandler };
