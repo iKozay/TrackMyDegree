3521c7aac2081100c55e176481c264ede0a1dc2a
Mohadgb <saidimoha.pro@gmail.com>
2025-10-11 17:18:58 -0400

    Tests POST /timeline/delete

--- Back-End/tests/timeline.test.js
+++ Back-End/tests/timeline.test.js
@@ -58,3 +58,36 @@ describe('POST /timeline/getAll', () => {
     expect(response.body).toHaveProperty('error', 'User ID is required');
   });
 });
+
+describe('POST /timeline/delete', () => {
+  it('should delete timeline successfully', async () => {
+    const request_body = { timeline_id: '1' };
+    timelineController.removeUserTimeline.mockResolvedValue(`Timeline with id: 1 deleted successfully`);
+    const response = await request(app).post('/timeline/delete').send(request_body).expect(200);
+    expect(response.body).toHaveProperty('message', `Timeline with id: 1 deleted successfully`);
+  });
+
+  it('should return 404 when timeline item not found', async () => {
+    const invalid_request = { timeline_id: 'nonexistent_item' };
+    timelineController.removeUserTimeline.mockResolvedValue(`No timeline found with id: nonexistent_item`);
+    const response = await request(app).post('/timeline/delete').send(invalid_request).expect(404);
+    expect(response.body).toHaveProperty('error', `No timeline found with id: nonexistent_item`);
+  });
+
+  it('should return 404 when timeline_item_id is missing', async () => {
+    const response = await request(app).post('/timeline/delete').send({}).expect(404);
+    expect(response.body).toHaveProperty('error', 'Timeline ID is required');
+  });
+
+  it('should return 404 when timeline controller returns error', async () => {
+    timelineController.removeUserTimeline.mockResolvedValue('Error occurred while deleting timeline.');
+    const response = await request(app).post('/timeline/delete').send({ timeline_id: '1' }).expect(404);
+    expect(response.body).toHaveProperty('error', 'Internal Server Error');
+  });
+
+  it('should return 500 when timeline controller triggers error in route function', async () => {
+    timelineController.removeUserTimeline.mockRejectedValueOnce(new Error('Database error'));
+    const response = await request(app).post('/timeline/delete').send({ timeline_id: '1' }).expect(500);
+    expect(response.body).toHaveProperty('error', 'Failed to delete timeline');
+  });
+});
