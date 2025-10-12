78637af6fe31cd36cb694810e81bd4f074ef89fe
sadeeshadman <145075448+sadeeshadman@users.noreply.github.com>
2025-10-12 13:32:18 -0600

    Delete Back-End/models/coursePoolModel.ts

--- Back-End/models/coursePoolModel.ts
+++
@@ -1,9 +0,0 @@
-import { model, Schema, Document } from 'mongoose';
-
-// A minimal schema just for existence checks
-const CoursePoolSchema: Schema = new Schema({
-  id: { type: String, required: true, unique: true },
-  // ... other course pool fields would go here
-});
-
-export default model('CoursePool', CoursePoolSchema);
