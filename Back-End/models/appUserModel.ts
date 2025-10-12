6aa6189e9cda4dbcf485d07abc17278e898a41c9
sadeeshadman <145075448+sadeeshadman@users.noreply.github.com>
2025-10-12 13:31:49 -0600

    Delete Back-End/models/appUserModel.ts

--- Back-End/models/appUserModel.ts
+++
@@ -1,9 +0,0 @@
-import { model, Schema, Document } from 'mongoose';
-
-// A minimal schema just for existence checks
-const AppUserSchema: Schema = new Schema({
-  id: { type: String, required: true, unique: true },
-  // ... other user fields would go here
-});
-
-export default model('AppUser', AppUserSchema);
