5288a071c9e42317524ad7d3dbf0c9d7b4ce3615
AbdelmalekMouhamou <146683074+AbdelmalekMouhamou@users.noreply.github.com>
2025-10-24 23:58:33 -0400

    Update AcceptanceUtils.js
    - the data sent to the timeline is now structured the same way as the data sent from the transcript page.
    - added extraction for the total credits required
    - the text is now parsed as a whole instead of per page to improve readability also page 1 is now ignored because it contains no relevant info
    - other readability improvements

--- front-end/src/utils/AcceptanceUtils.js
+++ front-end/src/utils/AcceptanceUtils.js
@@ -1,7 +1,6 @@
 import { pdfjs } from 'react-pdf';
 
 //Reads the PDF as an array buffer. The text is extracted using PDF.js
-// TODO: Move processing logic to seeparate utility file
 const parsePdfFile = (file) => {
   // Set the worker source for PDF.js
   pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.mjs`;
@@ -9,29 +8,25 @@ const parsePdfFile = (file) => {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
 
-    reader.onload = (e) => {
-      const typedArray = new Uint8Array(e.target.result);
+    reader.onload = async (e) => {
+      try {
+        const typedArray = new Uint8Array(e.target.result);
+        const pdf = await pdfjs.getDocument(typedArray).promise;
 
-      pdfjs
-        .getDocument(typedArray)
-        .promise.then((pdf) => {
-          let pagesPromises = [];
-          for (let i = 1; i <= pdf.numPages; i++) {
-            pagesPromises.push(
-              pdf.getPage(i).then((page) => {
-                return page.getTextContent().then((textContentPage) => {
-                  return {
-                    page: i,
-                    text: textContentPage.items.map((item) => item.str).join(' '),
-                  };
-                });
-              }),
-            );
-          }
+        let allText = '';
 
-          Promise.all(pagesPromises).then(resolve).catch(reject);
-        })
-        .catch(reject);
+        // Start from page 2 to skip the first page
+        for (let i = 2; i <= pdf.numPages; i++) {
+          const page = await pdf.getPage(i);
+          const textContent = await page.getTextContent();
+          const pageText = textContent.items.map((item) => item.str).join(' ');
+          allText += pageText;
+        }
+
+        resolve(allText);
+      } catch (error) {
+        reject(error);
+      }
     };
 
     reader.onerror = reject;
@@ -43,100 +38,66 @@ const parsePdfFile = (file) => {
 //This function generates all terms between the start end expected end terms
 //Returns a structured object
 // TODO: Refactor to simplify this complex function
-const extractAcceptanceDetails = (pagesData) => {
+const extractAcceptanceDetails = (text) => {
   const details = {};
 
   let results = [];
 
-  let offer_of_Admission = false;
-  pagesData.forEach(({ text }) => {
-    if (!pagesData || pagesData.length === 0) {
-      console.error('No pages data available');
-      return { results: [] };
-    }
-    // Check if text contains "OFFER OF ADMISSION"
-    if (text.toUpperCase().match('OFFER OF ADMISSION')) {
-      offer_of_Admission = true;
-    }
-
-    // Extract Degree Concentration (everything after Program/Plan(s) and before Academic Load)
-    const degreeConcentrationMatch = text.match(/Program\/Plan\(s\):\s*([^\n]+)(?:\n([^\n]+))?[\s\S]*?Academic Load/);
-
-    if (degreeConcentrationMatch) {
-      // Combine the two parts (if any) into a single Degree Concentration string
-      details.degreeConcentration = (degreeConcentrationMatch[1] + (degreeConcentrationMatch[2] || '')).trim(); // Add Degree Concentration to details
-    }
-    // Check for "Co-op Recommendation: Congratulations!"
-    const coopRecommendationMatch = text.match(/Co-op Recommendation:\s*Congratulations!/);
-    if (coopRecommendationMatch) {
-      details.coopProgram = 'Yes';
-      details.extendedCreditProgram = 'No'; //I don't think this is always true
-    } else {
-      // Extract Extended Credit Program or Co-op Program if not determined by Co-op Recommendation
-      // TODO: Check with the advisor if the ecp and coop are mutually exclusive
-      const ecpMatch = text.match(/Extended Credit Program/);
-      const coopMatch = text.match(/Co-op Program/);
-      if (ecpMatch) {
-        details.extendedCreditProgram = 'yes';
-        details.coopProgram = 'No';
-      } else if (coopMatch) {
-        details.coopProgram = 'Yes';
-        details.extendedCreditProgram = 'No';
-      }
-    }
-    // Extract Starting Semester (Term)
-    extractTermFromText({
-      text,
-      startLabel: 'Session',
-      endLabel: 'Minimum Program Length',
-      targetField: 'startingTerm',
-      details,
-    });
-    // Extract Expected Graduation Term (Winter 2024, Fall/Winter 2023-2024)
-    extractTermFromText({
-      text,
-      startLabel: 'Expected Graduation Term',
-      endLabel: 'Admission Status',
-      targetField: 'expectedGraduationTerm',
-      details,
-    });
-    // Extract Exempted Courses 
-    extractCoursesFromText({
-      text,
-      startLabel: 'Exemptions',
-      endLabel: 'Deficiencies',
-      courseType: 'Exempted',
-      results,
-      targetField: 'exemptionsCourses',
-      details,
-    });
-
-    // Extract Deficiency Courses
-    extractCoursesFromText({
-      text,
-      startLabel: 'Deficiencies',
-      endLabel: 'Transfer Credits',
-      courseType: 'Deficiency',
-      results,
-      targetField: 'deficienciesCourses',
-      details,
-    });
-    // Extract Transfer Credits
-    extractCoursesFromText({
-      text,
-      startLabel: 'Transfer Credits',
-      endLabel: 'NOTE:',
-      courseType: 'Transfered Courses',
-      results,
-      targetField: 'transferCredits',
-      details,
-    });
-  }); // pages end
-
-  if (!offer_of_Admission) {
-    alert('Please choose Offer of Admission');
-    return { results: [] };
+  if (!text || text.length === 0) {
+    console.error('No pages data available');
+    return { results, details };
   }
+  // Check if text contains "OFFER OF ADMISSION"
+  if (!text.toUpperCase().match('OFFER OF ADMISSION')) {
+    alert('Please choose Offer of Admission');
+    return { results, details };
+  }
+
+  // Extract Degree Concentration (everything after Program/Plan(s) and before Academic Load)
+  const degreeConcentrationMatch = text.match(/Program\/Plan\(s\):\s*([^\n]+)(?:\n([^\n]+))?[\s\S]*?Academic Load/);
+
+  if (degreeConcentrationMatch) {
+    // Combine the two parts (if any) into a single Degree Concentration string
+    details.degreeConcentration = (degreeConcentrationMatch[1] + (degreeConcentrationMatch[2] || '')).trim(); // Add Degree Concentration to details
+  }
+  // Check for "Co-op Recommendation: Congratulations!"
+  if (text.match(/Co-op Recommendation:\s*Congratulations!/) || text.match(/Co-op Program/)) {
+    details.coopProgram = true;
+  }
+
+  if (text.match(/Extended Credit Program/)) {
+    details.extendedCreditProgram = true;
+  }
+  // Extract Starting Semester (Term)
+  details.startingTerm = extractTermFromText({
+    text,
+    startLabel: 'Session',
+    endLabel: 'Minimum Program Length',
+  });
+  // Extract Expected Graduation Term (Winter 2024, Fall/Winter 2023-2024)
+  details.expectedGraduationTerm = extractTermFromText({
+    text,
+    startLabel: 'Expected Graduation Term',
+    endLabel: 'Admission Status',
+  });
+
+  details.minimumProgramLength = text.match(/Minimum Program Length:\s*(\d+)\s*credits?/i)?.[1] || null;
+
+  // Extract Exempted Courses
+  const exemptions = getCoursesFromText({ text, startLabel: 'Exemptions:', endLabel: 'Deficiencies:' });
+  if (exemptions.length > 0) results.push({ term: 'Exempted', courses: exemptions });
+
+  // Extract Deficiency Courses
+  const deficiencies = getCoursesFromText({ text, startLabel: 'Deficiencies:', endLabel: 'Transfer Credits:' });
+  if (deficiencies.length > 0) results.push({ term: 'Deficiencies', courses: deficiencies });
+
+  // Extract Transfer Credits
+  const transferCredits = getCoursesFromText({
+    text,
+    startLabel: 'Transfer Credits:',
+    endLabel: 'ADDITIONAL INFORMATION',
+  });
+  if (transferCredits.length > 0) results.push({ term: 'Transfered Courses', courses: transferCredits });
 
   // Generate all terms from startingTerm to expectedGraduationTerm
   const terms = generateTerms(details.startingTerm, details.expectedGraduationTerm);
@@ -144,14 +105,13 @@ const extractAcceptanceDetails = (pagesData) => {
     results.push({
       term: term.trim(),
       course: '',
-      grade: 'EX',
     });
   });
 
   return { results, details };
 };
 //Helper function to extract terms between two labels (text markers in the document that indicate the start and end of a section)
-function extractTermFromText({ text, startLabel, endLabel, targetField, details }) {
+function extractTermFromText({ text, startLabel, endLabel }) {
   const section = getSectionBetweenLabels(text, startLabel, endLabel);
   if (!section) return;
   // Regex for matching academic terms like "Winter 2024" or "Fall/Winter 2023-2024"
@@ -159,45 +119,31 @@ function extractTermFromText({ text, startLabel, endLabel, targetField, details 
   const match = section.match(termRegex);
 
   if (match) {
-    details[targetField] = match[0].trim();
+    return match[0].trim();
   }
+  return null;
 }
 //Helper function to extract courses between two labels (text markers in the document that indicate the start and end of a section)
-function extractCoursesFromText({ text, startLabel, endLabel, courseType, results, targetField, details }) {
+function getCoursesFromText({ text, startLabel, endLabel, courseType }) {
   // Regex for matching courses (e.g., COMM A, ECON 201)
   const courseRegex = /[A-Z]{3,4}\s+\d{3}/g;
-  const creditRegex = /\b\d{1,5}\s{1,3}crs\b/; // Regex for matching credits (e.g., "3 crs")
   const sectionText = getSectionBetweenLabels(text, startLabel, endLabel);
   const courses = [];
   let match;
-
   while ((match = courseRegex.exec(sectionText)) !== null) {
     const course = match[0].trim();
-    let courseInfo = course;
-    if (courseType !== 'Deficiency') {
-      results.push({
-        course: course.replace(/\s+/g, ''),
-        term: courseType,
-        grade: 'EX',
-      });
-    }
-    //if it's a transfer course, look for credits
-    if (courseType === 'Transfered Courses') {
-      const lineContainingCourse = sectionText.split('\n').find((line) => line.includes(course));
-      const creditMatch = lineContainingCourse?.match(creditRegex);
-      const credits = creditMatch ? `${creditMatch[1]} crs` : 'Credits not found';
-      courseInfo = { course, credits };
-    }
-    courses.push(courseInfo);
+    courses.push(course.replace(/\s+/g, ''));
   }
-  if (courses.length > 0) details[targetField] = courses;
+  return courses;
 }
+
 function getSectionBetweenLabels(text, startLabel, endLabel) {
   const startIndex = text.indexOf(startLabel);
   const endIndex = text.indexOf(endLabel);
   if (startIndex === -1 || endIndex === -1) return null;
   return text.substring(startIndex, endIndex);
 }
+
 // Function to generate all terms from startTerm to endTerm
 const generateTerms = (startTerm, endTerm) => {
   const terms = ['Winter', 'Summer', 'Fall'];
