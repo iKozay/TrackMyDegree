59758d2e03278b3ca60720198e6260b24ae80f4c
ChrisAnhThien <nathien2310@gmail.com>
2025-10-26 12:28:55 -0400

    #117 - Exclude Utils from coverage

--- front-end/jest.config.js
+++ front-end/jest.config.js
@@ -10,4 +10,19 @@ module.exports = {
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleDirectories: ['node_modules', 'src'],
+
+  // Only collect coverage from page components (or even just UserPage.js)
+  collectCoverageFrom: [
+    'src/pages/**/*.js',     // or: 'src/pages/UserPage.js'
+    '!src/pages/**/__tests__/**',
+  ],
+
+  // Explicitly ignore utils from coverage calculations
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '<rootDir>/src/utils/',   // ignore all utils
+  ],
+
+  // (optional) show nicer reports and enforce a bar
+  coverageReporters: ['text', 'lcov', 'html'],
 };
