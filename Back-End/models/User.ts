1c32df2734ec0b17b9293de7c26b19fab3133e0a
Mohadgb <saidimoha.pro@gmail.com>
2025-10-30 20:14:20 -0400

    #140 update the User model

--- Back-End/models/User.ts
+++ Back-End/models/User.ts
@@ -14,6 +14,11 @@ const UserSchema = new Schema({
   type: { type: String, enum: ['student', 'advisor', 'admin'], required: true },
   otp: { type: String, default: null },
   otpExpire: { type: Date, default: null },
+
+  // 🔹 Added fields for password reset token
+  resetToken: { type: String, default: null },
+  resetTokenExpire: { type: Date, default: null },
+
   deficiencies: [DeficiencySchema],
   exemptions: [{ type: String, ref: 'Course' }]
 });
