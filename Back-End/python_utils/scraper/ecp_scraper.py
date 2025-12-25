90531f006e2a0921836415bb82d820cb3a080700
Omar <omar.hammodan@gmail.com>
2025-12-25 13:29:59 -0500

    scraped core course pool for engr ecp

--- Back-End/python_utils/scraper/ecp_scraper.py
+++ Back-End/python_utils/scraper/ecp_scraper.py
@@ -3,6 +3,7 @@ This scraper scrapes two degrees: one for COMP ECP and another for ENGR ECP.
 '''
 from . import course_data_scraper
 from . import engr_general_electives_scraper
+from urllib.parse import urljoin
 
 
 def scrape_engr_ecp():
@@ -19,5 +20,20 @@ def scrape_engr_ecp():
     soup = course_data_scraper.fetch_html(URL)
     def_group = soup.find('div', class_='defined-group')
     tr = def_group.findAll('tr')
-    print(tr[1])
+    
     #Core courses
+    course_pool.append({
+        '_id': "ECP_ENGR_Core",
+        'name': "ECP_ENGR_Core",
+        'creditsRequired': 18,
+        'courses':[]
+    })
+    core_course_list=tr[1].findAll('div', class_='formatted-course')
+    for course in core_course_list:
+        course_code = course.find('a').text
+        courses.append(course_data_scraper.extract_course_data(course_code,urljoin(URL, course.find('a').get('href'))))
+        course_pool[0]["courses"].append(course_code)
+
+    for course in courses:
+        print(course)
+        print('\n')
