0305fc5aa57b188e4f2177005c3f124824578f57
Omar <omar.hammodan@gmail.com>
2025-12-29 12:07:54 -0500

    added exclusion list for comp ecp electives

--- Back-End/python_utils/scraper/ecp_scraper.py
+++ Back-End/python_utils/scraper/ecp_scraper.py
@@ -66,3 +66,53 @@ def scrape_engr_ecp():
     courses+=gen_electives[1]
 
     return [degree, course_pool, courses]
+
+def scrape_comp_ecp():
+    URL="https://www.concordia.ca/academics/undergraduate/calendar/current/section-71-gina-cody-school-of-engineering-and-computer-science/section-71-70-department-of-computer-science-and-software-engineering/section-71-70-3-extended-credit-program.html"
+    degree={
+            '_id': "COMP_ECP",
+            'name': "COMP Extended Credits Program",
+            'totalCredits': 30,
+            'coursePools': []
+        }
+    course_pool=[]
+    courses=[]
+
+    soup = course_data_scraper.fetch_html(URL)
+    def_group = soup.find('div', class_='defined-group')
+    tr = def_group.findAll('tr')
+
+    #Core courses
+    course_pool.append({
+        '_id': "ECP_COMP_Core",
+        'name': "ECP_COMP_Core",
+        'creditsRequired': 9,
+        'courses':[]
+    })
+
+    core_course_list=add_courses(tr[1].findAll('div', class_='formatted-course'), URL)
+    course_pool[0]['courses']+=core_course_list[0]
+    courses+=core_course_list[1]
+
+    # General Electives
+    course_pool.append({
+        '_id': "ECP_ENGR_General_Electives",
+        'name': "ECP_ENGR_General_Electives",
+        'creditsRequired': 6,
+        'courses':[]
+    })
+
+    exclusion_list_html=soup.find('div', class_='defined-group', title='ECP Electives Exclusion List')
+    exclusion_list=add_courses(exclusion_list_html.findAll('div', class_='formatted-course'), URL)
+
+    gen_electives = engr_general_electives_scraper.scrape_electives()
+    course_pool[1]['courses']+=gen_electives[0]
+    courses+=gen_electives[1]
+
+    print(exclusion_list[0])
+
+    for course in exclusion_list[1]:
+        if course["code"] in course_pool[1]['courses']:
+            course_pool[1]['courses'].remove(course['code'])
+            courses.remove(course)
+
