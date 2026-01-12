f426e754c3f02fe6d722d00d5cf1c151cd2dad8f
Omar <omar.hammodan@gmail.com>
2026-01-11 20:20:25 -0500

    giving in which terms a course is given

--- Back-End/python_utils/scraper/concordia_api_utils.py
+++ Back-End/python_utils/scraper/concordia_api_utils.py
@@ -9,17 +9,19 @@ API_KEY=os.getenv("CONCORDIA_KEY")
 
 TERM=["0", "Summer", "Fall", "Fall/Winter", "Winter", "Spring (for CCCE career only)", "Summer (for CCCE career only)"]
 
-def get_term(term_int):
-    for i in range(3):
-        term_int = term_int % pow(10, 3-i)
-    
-    return TERM[term_int]
+def get_term(course_code):
+    subject_and_catalog=course_code.split()
+    terms=[]
 
-print(get_term(1234))
+    response = requests.get(
+        f"https://opendata.concordia.ca/API/v1/course/section/filter/{subject_and_catalog[0]}/{subject_and_catalog[1]}",
+        auth=(API_USER, API_KEY)
+    ).json()
 
-'''response = requests.get(
-    "https://opendata.concordia.ca/API/v1/course/section/filter/PHYS/226",
-    auth=("951", "141faa6235c1e3e06ad8df13bcaa156b")
-)
-
-print(response.json())'''
+    for dict in response:
+        term_int=int(dict["term"])
+        for i in range(3):
+            term_int = term_int % pow(10, 3-i)
+        
+        terms.append(TERM[term_int])
+    return list(set(terms))
