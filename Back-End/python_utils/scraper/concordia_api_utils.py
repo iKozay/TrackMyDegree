ef7bef3191e9bc1a5a8e6cf8d033a75ccc5f770d
Omar <omar.hammodan@gmail.com>
2026-01-11 19:01:44 -0500

    translating term code for database use

--- Back-End/python_utils/scraper/concordia_api_utils.py
+++ Back-End/python_utils/scraper/concordia_api_utils.py
@@ -7,7 +7,15 @@ load_dotenv()
 API_USER=os.getenv("CONCORDIA_USER")
 API_KEY=os.getenv("CONCORDIA_KEY")
 
-print(API_USER)
+TERM=["0", "Summer", "Fall", "Fall/Winter", "Winter", "Spring (for CCCE career only)", "Summer (for CCCE career only)"]
+
+def get_term(term_int):
+    for i in range(3):
+        term_int = term_int % pow(10, 3-i)
+    
+    return TERM[term_int]
+
+print(get_term(1234))
 
 '''response = requests.get(
     "https://opendata.concordia.ca/API/v1/course/section/filter/PHYS/226",
