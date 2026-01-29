32fdb992e0c3f83ab6fb711378324f708d06a8a1
Mohadgb <saidimoha.pro@gmail.com>
2026-01-28 20:36:24 -0500

    coopValidation

--- Back-End/src/routes/coopvalidationRoutes.ts
+++ Back-End/src/routes/coopvalidationRoutes.ts
@@ -1,6 +1,6 @@
 import { Router, Request, Response } from 'express';
 import redisClient from '@/config/redis';
-import { validateCoopTimeline } from '@/services/coop/coopvalidationService';
+import { validateCoopTimeline } from '@/services/coop/coopvalidationService.';
 
 const router = Router();
 
