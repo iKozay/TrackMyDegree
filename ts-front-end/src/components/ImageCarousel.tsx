f78257d500ad6555334f18527f54a617a8aed144
codedsami <miskat.mahmud@mail.concordia.ca>
2026-01-29 20:15:09 -0500

    moving carousel file and creating new css folder in tsfrontend

---
+++ ts-front-end/src/components/ImageCarousel.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import "../css/LandingPage.css";
+import pic1 from '../images/courselistpage.png';
+import pic2 from '../images/uploadAcceptanceletter.png';
+import pic4 from '../images/timelinepage.png';
+
+const images = [pic1, pic2, pic4];
+
+const ImageCarousel = () => {
+  return (
+    <Carousel pause={false} data-bs-theme="dark" className="carousel-surround">
+      {images.map((image, index) => (
+        <Carousel.Item key={index}>
+          <img className="carousel-img" src={image} alt={`Slide ${index + 1}`} />
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
+};
+
+export default ImageCarousel;
