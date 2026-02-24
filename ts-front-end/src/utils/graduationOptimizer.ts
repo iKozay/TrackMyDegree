a7578009bed560b041610a8f3cbaea08e3313249
Mohamed Saidi <msaidi@biximontreal.com>
2026-02-24 14:48:31 -0500

    feat(optimizer): init graduationOptimizer module with imports

---
+++ ts-front-end/src/utils/graduationOptimizer.ts
@@ -0,0 +1,10 @@
+import type {
+  TimelineState,
+  Course,
+  Semester,
+  SemesterId,
+  CourseMap,
+} from "../types/timeline.types";
+
+const MAX_CREDITS_PER_SEMESTER = 19;
+const MAX_EXTRA_SEMESTERS = 24;
