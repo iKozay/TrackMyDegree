35b3bcac312e749b7bc4ba6481902f473d93008a
Mohamed Saidi <msaidi@biximontreal.com>
2026-03-20 16:30:07 -0400

    chore(shared): add CreateDegreeInput, CreateCoursePoolInput and update types

--- shared/types/degree.ts
+++ shared/types/degree.ts
@@ -6,9 +6,27 @@ export interface DegreeData {
   coursePools?: string[];
   degreeType?: string;
 }
+
 export interface CoursePoolInfo {
   _id: string;
   name: string;
   creditsRequired: number;
   courses: string[];
 }
+
+export interface CreateDegreeInput {
+  name: string;
+  totalCredits: number;
+  degreeType?: string;
+}
+
+export type UpdateDegreeInput = Partial<CreateDegreeInput>;
+
+export interface CreateCoursePoolInput {
+  name: string;
+  creditsRequired: number;
+  degreeId: string;
+  courses: string[];
+}
+
+export type UpdateCoursePoolInput = Partial<Omit<CreateCoursePoolInput, 'degreeId'>>;
