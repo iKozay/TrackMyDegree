c99f89a0fe2cd0ea78796c796afa46a7c59628db
Zeiad Badawy <zeiadbadawy@gmail.com>
2026-04-12 22:04:32 -0400

    Revert "Update ts-front-end/src/tests/pages/EulaPage.test.tsx"
    This reverts commit a7cac0aa971487d3e003d31f8a18a7bad39b2538.

--- ts-front-end/src/tests/pages/EulaPage.test.tsx
+++ ts-front-end/src/tests/pages/EulaPage.test.tsx
@@ -9,8 +9,8 @@ describe("EulaPage", () => {
     expect(
       screen.getByRole("heading", { name: "User Consent and End-User License Agreement (EULA)" })
     ).toBeInTheDocument();
-    expect(screen.getByText(/Effective Date:\s*2025-10-10/)).toBeInTheDocument();
-    expect(screen.getByText(/Last Updated:\s*2026-04-12/)).toBeInTheDocument();
+    expect(screen.getByText("2025-10-10")).toBeInTheDocument();
+    expect(screen.getByText("2026-04-12")).toBeInTheDocument();
     expect(screen.getByRole("link", { name: "1. Introduction" })).toHaveAttribute(
       "href",
       "#section-1-introduction"
